8d765ca59d1b56bc8c6d80638305e639357e0d06
Scott Tancock <st12660@my.bristol.ac.uk>
2018-05-24 17:33:20 +0100

    Write to file /tmp/tdc.dat or other given file

--- dpticat.cpp
+++ dpticat.cpp
@@ -1,6 +1,9 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 
+#include <iostream>
+#include <string>
+#include <fstream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
@@ -20,12 +23,15 @@
 #define W_BYTES 8
 #define WIDTH (W_BYTES * 8)
 #define FINE 10
-#define COARSE (WIDTH - FINE)
+#define TRAIN 8
+#define COARSE (WIDTH - FINE - TRAIN)
+#define TRAIN_SEQ 0x6A
 
 typedef unsigned char byte;
 
 char dev_name[cchDvcNameMax + 1];
 HIF hif = hifInvalid;
+unsigned char train_seq[TRAIN/8] = {TRAIN_SEQ};
 
 void cancel_out(int signum){
   fprintf(stderr, "Ctrl-C pressed, exiting...\n");
@@ -50,13 +56,13 @@ int main( int argc, char* argv[] ) {
     fflush(stderr);
     exit(2);
   }
-  printf("Opened HIF\n");
+  fprintf(stderr, "Opened HIF\n");
   signal(SIGINT, &cancel_out);
   int n_ports;
   
   DptiGetPortCount(hif, &n_ports);
   
-  printf("Number of ports on %s: %i\n", dev_name, n_ports);
+  fprintf(stderr, "Number of ports on %s: %i\n", dev_name, n_ports);
   
   if(argc < 3){
     fprintf(stderr, "ERROR: no port specified");
@@ -70,10 +76,10 @@ int main( int argc, char* argv[] ) {
   DptiGetPortProperties(hif, port_num, &port_props);
   
   if(port_props & dprpPtiAsynchronous){
-    printf("Port %i is asynchronous\n", port_num);
+    fprintf(stderr, "Port %i is asynchronous\n", port_num);
   }
   else{
-    printf("Port %i is synchronous\n", port_num);
+    fprintf(stderr, "Port %i is synchronous\n", port_num);
   }
   
   if (!DptiEnableEx(hif, port_num)){
@@ -82,14 +88,23 @@ int main( int argc, char* argv[] ) {
     fflush(stderr);
     exit(4);
   }
+
+  std::string filename;
+  std::ofstream file;
   
+  if (argc < 4) {
+    file = std::ofstream("/tmp/tdc.dat", std::ios_base::trunc);
+  }
+  else {
+    file = std::ofstream(argv[3], std::ios_base::trunc);
+  }
   
   int n_bytes = 128;
   byte *out_bytes = new byte[1];
   byte *in_bytes = new byte[n_bytes];
   
   for(int test_count = 0; test_count < N_TESTS; test_count++){
-    printf("Test %i\n", test_count);
+    fprintf(stderr, "Test %i\n", test_count);
     //int n_bytes = rand() & 0xFF;
     out_bytes[0] = n_bytes;
     //out_bytes[1] = 255 - n_bytes;
@@ -119,23 +134,35 @@ int main( int argc, char* argv[] ) {
     unsigned int prev_fine = 0;
     double prev_time = 0;
     
-    for(int i = 0; i < out_bytes[0]; i+=W_BYTES){
-      for(int j = 0; j < W_BYTES; j++){
+    for(int i = 0; (i+W_BYTES) <= out_bytes[0]; i+=W_BYTES){
+      fprintf(stderr, "i = %i\n", i);
+      int j;
+      for(j = 0; j < W_BYTES; j++){
 	printf("%02x", in_bytes[i+j]);
+      }
+      for(j = 0; j*8 < TRAIN; j++){
+	if(in_bytes[i+j] != train_seq[j]){
+	  fprintf(stderr, "Miss.\n");
+	  i -= W_BYTES-1;
+	  break;
+	}
+      }
+      if(j * 8 < TRAIN){
+	continue;
       }
       printf("\n");
       unsigned long long curr_coarse = 0;
-      for(int j = 0; j+8 <= COARSE; j += 8){
-	curr_coarse = curr_coarse << 8 | in_bytes[i+(j >> 3)];
+      for(j = (TRAIN >> 3); (j+1)*8 <= COARSE+TRAIN; j++){
+	curr_coarse = curr_coarse << 8 | in_bytes[i+j];
       }
-      int leftover = COARSE & 0x7;
+      int leftover = (COARSE+TRAIN) & 0x7;
       if(leftover){
-	printf("Coarse before: %llu, extra: %x\n", curr_coarse << leftover, (in_bytes[i + (COARSE >> 3)] >> (8-leftover)));
-	curr_coarse = curr_coarse << leftover  | (in_bytes[i + (COARSE >> 3)] >> (8-leftover));
+	printf("Coarse before: %llu, extra: %x\n", curr_coarse << leftover, (in_bytes[i + ((COARSE + TRAIN) >> 3)] >> (8-leftover)));
+	curr_coarse = curr_coarse << leftover  | (in_bytes[i + ((COARSE+TRAIN) >> 3)] >> (8-leftover));
       }
       unsigned int curr_fine = 0;
-      for(int j = 0; j+8 <= FINE; j += 8){
-	curr_fine = curr_fine | (((unsigned int)in_bytes[i+W_BYTES-1-(j>>3)]) << j);
+      for(j = 0; (j+1)*8 <= FINE; j++){
+	curr_fine = curr_fine | (((unsigned int)in_bytes[i+W_BYTES-1-j]) << (j*8));
       }
       leftover = FINE & 0x7;
       if(leftover){
@@ -144,12 +171,13 @@ int main( int argc, char* argv[] ) {
       double curr_time = ((curr_coarse+1) / 120000000.0) - (curr_fine * 0.000000000015);
       printf("Coarse: %llu, fine: %u, time: %lf\n", curr_coarse, curr_fine, curr_time);
       printf("CDiff: %llu, FDiff: %i, TDiff: %le\n", curr_coarse - prev_coarse, (int)curr_fine - (int)prev_fine,curr_time - prev_time);
+      file << curr_coarse << "," << curr_fine << "," << curr_time << "," << curr_time - prev_time << std::endl;
       prev_coarse = curr_coarse;
       prev_fine = curr_fine;
       prev_time = curr_time;
     }
   }
-  
+  file.close();
   DmgrClose(hif);
   return 0;
 }
